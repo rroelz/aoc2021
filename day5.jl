ab91c4db0f30885ce44b8ece67f531e3e16f2cc6
rroelz <robin.roelz@gmail.com>
2021-12-05 13:33:38 +0100

    finished day 5

---
+++ day5.jl
@@ -0,0 +1,31 @@
+input = readlines("input/5")
+ventmap = zeros(Int, 1000, 1000)
+for line in input
+    a, b = split(line, " -> ")
+
+    x1, y1 = parse.(Int64, split(a, ",")).+1
+    x2, y2 = parse.(Int64, split(b, ",")).+1
+
+    if (x1==x2) | (y1==y2)
+        ventmap[x1:x2, y1:y2].+=1
+        ventmap[x1:-1:x2, y1:-1:y2].+=1
+    else
+        xcoords = collect(range(x1,x2, step = 1))
+        ycoords = collect(range(y1,y2, step = 1))
+        
+        if isempty(xcoords) 
+            xcoords = collect(range(x1, x2, step = -1))
+        end
+        if isempty(ycoords) 
+            ycoords = collect(range(y1, y2, step = -1))
+        end
+        coords = []
+        for i in 1:length(xcoords)
+            ventmap[xcoords[i], ycoords[i]] +=1
+        end
+    end 
+end
+
+overlap = count(i -> (i>=2), ventmap)
+ventmap
+print("Black smokers overlap at $overlap points")
